9a8b239321ca8ecf95dae7b1f34a82a29949b582
Enzo <enzomiguelrf@gmail.com>
2026-09-18 11:19:33 -0300

    Aula02

--- Aula02/conceito_tipos_de_dados.js
+++ Aula02/conceito_tipos_de_dados.js
@@ -10,4 +10,25 @@
 // Null (Valor Vazio)
 // Undefined (Quando algo Não foi Definido)
 
-//
+
+
+// STRING (Textos)
+// Uma String é um texto, sempre escrito entre aspas ("" ou '')
+let nome = "Jarvis";
+let mensagem = 'olá, mundo!';
+
+console.log(nome);
+console.log(mensagem);
+
+let saudacao = "Olá, " + nome + "!";
+console.log(saudacao);//exibe "olá, Jarvis!"
+
+// typeof
+// O typeof serve para descobrir o tipo de um valor ou variável
+
+//let nomeDois = "Bryan";
+//let nomeTres = "Vilar";
+//console.log(typeof nomeDois);
+
+let soma = 10 + 5;
+console.log(soma);
---
+++ Aula02/exercicio_01.js
@@ -0,0 +1,9 @@
+// Atividade Number
+// Crie uma variável chamada anoAtual e outra chamada anoNascimento.
+// calcule e exiba a idade no(a) console.
+
+let anoAtual = 2026
+let anoNascimento = 2010
+
+let subtração = anoAtual - anoNascimento;
+console.log(subtração)
